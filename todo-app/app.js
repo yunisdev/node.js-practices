6a8e6bc3d58e91f905f82c109a0e46f66f616b2b
Yunis Huseynzade <yunisdev.04@gmail.com>
2020-02-22 23:50:54 +0400

    todo half

--- todo-app/app.js
+++ todo-app/app.js
@@ -1,6 +1,7 @@
 const json = 'todo.json'
 const fs = require('fs');
 const chalk = require('chalk');
+const yargs = require('yargs');
 
 const getData = ()=>{
     return JSON.parse(fs.readFileSync(json).toString())
@@ -18,5 +19,44 @@ const addTODO = (text)=>{
     writeData(data);
 }
 const isDone = (text)=>{
-    
+    const data = getData();
+    for(var i=0;i<data.data.length;i++){
+        if(data.data[i].text==text){
+            data.data[i].isDone = true;
+        }
+    }
+    writeData(data);
 }
+const listTODO = ()=>{
+    const data = getData();
+    for(var i=0;i<data.data.length;i++){
+        var a = data.data[i];
+        if(a.isDone==true){
+            console.log(chalk.green(a.text));
+        }else{
+            console.log(chalk.blue(a.text));
+        }
+    }
+}
+yargs.command({
+    command:'add',
+    describe:'Add TODO',
+    handler:(argv)=>{
+        addTODO(argv.title);
+    }
+}) 
+yargs.command({
+    command:'list',
+    describe:'List TODOs',
+    handler:()=>{
+        listTODO();
+    }
+})
+yargs.command({
+    command:'done',
+    describe:'Done this TODO',
+    handler:(argv)=>{
+        isDone(argv.title);
+    }
+})
+yargs.parse();
