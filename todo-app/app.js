c3970e605e995812405fa5fe2e293b1d7b10c2bd
Yunis Huseynzade <yunisdev.04@gmail.com>
2020-02-23 14:25:44 +0400

    todo finish

--- todo-app/app.js
+++ todo-app/app.js
@@ -9,20 +9,75 @@ const getData = ()=>{
 const writeData = (data)=>{
     fs.writeFileSync(json,JSON.stringify(data));
 }
-
-const addTODO = (text)=>{
+const refresh =  ()=>{
     const data = getData();
-    data.data.push({
-        text:text,
-        isDone:false,
-    });
+    data.data = [];
     writeData(data);
 }
+const howManyDone = ()=>{
+    const data = getData();
+    var count = 0;
+    for(var i=0;i<data.data.length;i++){
+        if(data.data[i].isDone==true){
+            count++;
+        }
+    }
+    if(count==0){
+        return "None"
+    }
+    return count;
+}
+const dataExist = (text)=>{
+    const data = getData()
+    for(var i=0;i<data.data.length;i++){
+        if(data.data[i].text==text){
+            return true;
+        }
+    }
+    return false;
+}
+const addTODO = (text)=>{
+    const data = getData();
+    if(dataExist(text)==false){
+        data.data.push({
+            text:text,
+            isDone:false,
+        });
+        writeData(data);
+    }else{
+        console.log(chalk.red('You already have this TODO'));
+    }
+    console.log(chalk.yellow(`You have ${getData().data.length} TODOs.${howManyDone()} of them is done`));
+}
 const _todo = ()=>{
+    const data = getData();
+    var indexer=0;
     for(var i=0;i<data.data.length;i++){
         var a = data.data[i];
         if(a.isDone==false){
-            console.log(a.text);
+            indexer++
+            console.log(indexer+") "+a.text);
+        }
+    }
+}
+const removeData =(index)=>{
+    const data = getData();
+    var arr = [];
+    for(var i=0;i<data.data.length;i++){
+        if(index!=i){
+            arr.push(data.data[i]);
+        }
+    }
+    data.data = arr;
+    writeData(data);
+    console.log(chalk.yellow(`You have ${getData().data.length} TODOs.${howManyDone()} of them is done`));
+}
+const remove = (title)=>{
+    const data = getData();
+    for(var i=0;i<data.data.length;i++){
+        if(data.data[i].text==title){
+            removeData(i);
+            break;
         }
     }
 }
@@ -37,14 +92,17 @@ const isDone = (text)=>{
 }
 const listTODO = ()=>{
     const data = getData();
+    var indexer = 0;
     for(var i=0;i<data.data.length;i++){
         var a = data.data[i];
+        indexer++
         if(a.isDone==true){
-            console.log(chalk.green(a.text));
+            console.log(indexer+") "+chalk.green(a.text));
         }else{
-            console.log(chalk.blue(a.text));
+            console.log(indexer+") "+chalk.blue(a.text));
         }
-    }
+    }    
+    console.log(chalk.yellow(`You have ${getData().data.length} TODOs.${howManyDone()} of them is done`));
 }
 yargs.command({
     command:'add',
@@ -72,6 +130,20 @@ yargs.command({
     describe:'Thing to do',
     handler:()=>{
         _todo();
+    }
+})
+yargs.command({
+    command:'empty',
+    describe:'Erases all TODOs',
+    handler:()=>{
+        refresh();
+    }
+})
+yargs.command({
+    command:'remove',
+    describe:'Removes TODO',
+    handler:(argv)=>{
+        remove(argv.title);
     }
 })
 yargs.parse();
