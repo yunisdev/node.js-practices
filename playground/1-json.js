fa460cc2e138d35542e33186ac2a9a2fb5b42f7e
Yunis Huseynzade <yunisdev.04@gmail.com>
2020-02-23 14:26:09 +0400

    todo finiah

--- playground/1-json.js
+++
@@ -1,9 +0,0 @@
-const fs = require('fs');
-
-const data = JSON.parse(fs.readFileSync('1-json.json').toString());
-
-data.name = "Yusif";
-data.age = 16;
-const dataString = JSON.stringify(data);
-
-fs.writeFileSync('1-json.json',dataString);
