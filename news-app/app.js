4f57ec604b1c5e7450296211988339ffa66b8fa3
Yunis Huseynzade <yunisdev.04@gmail.com>
2020-02-28 12:30:43 +0400

    news

--- news-app/app.js
+++ news-app/app.js
@@ -1,27 +1,33 @@
 const request = require('request');
 const chalk = require('chalk');
 const yargs = require('yargs');
-const urlData = `http://newsapi.org/v2/everything?${yargs.argv.q?`q=${yargs.argv.q}&`:""}language=en&sources=google-news&apiKey=81895a771a294e1284b5c1d18d87f361`;
-const printNews=(data)=>{
+const urlData = `http://newsapi.org/v2/top-headlines?${yargs.argv.q ? `q=${yargs.argv.q}&` : ""}${yargs.argv.lang ? `language=${yargs.argv.lang}&` : ""}${yargs.argv.country ? `country=${yargs.argv.country}&` : ""}apiKey=81895a771a294e1284b5c1d18d87f361`;
+const printNews = (data) => {
     console.log(chalk.bgWhite.blue.inverse(data.title));
-    if(data.author!=null)console.log(chalk.green.inverse(data.author));
+    if (data.author != null) console.log(chalk.green.inverse(data.author));
     console.log(chalk.inverse(data.description));
-    console.log(chalk.inverse(`--To read more:`+chalk.inverse.yellow(`${data.url}\n`)));
+    console.log(chalk.inverse(`--To read more:` + chalk.inverse.yellow(`${data.url}\n`)));
 };
 request({
-    url:urlData,
-    json:true,
-},(error,response)=>{
-    if(error){
+    url: urlData,
+    json: true,
+}, (error, response) => {
+    if (error) {
         console.log('Unable to get news');
-    }else{
+    } else {
         const data = response.body;
-        console.clear();
-        for(var i=0;i<data.articles.length;i++){
-            if(i!=0){
-                console.log('===================================\n');
+        if (data.articles) {
+            console.clear();
+            for (var i = 0; i < data.articles.length; i++) {
+                if (i != 0) {
+                    console.log('===================================\n');
+                }
+                printNews(data.articles[i]);
             }
-            printNews(data.articles[i]);
+        } else {
+
+            console.log('Unable to get news');
         }
+
     }
 });
