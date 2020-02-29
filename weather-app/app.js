b697e3d3aca88a4211d298c2b67923c1cc8ba6e3
Yunis Huseynzade <yunisdev.04@gmail.com>
2020-02-29 19:49:36 +0400

    express

--- weather-app/app.js
+++ weather-app/app.js
@@ -2,19 +2,19 @@ const geoCode = require('./utils/geocode.js');
 const forecast = require('./utils/forecast.js');
 const address = process.argv[2];
 if (address) {
-    geoCode(address, (error, geoData) => {
+    geoCode(address, (error, {latitude,place_name,longitude}) => {
         if (error) {
             return console.log('Error: ', error);
         }
-        forecast(geoData.latitude, geoData.longitude, (error, data) => {
+        forecast(latitude, longitude, (error, {temperature,tempF,precipProbability,summary,hourlySum}) => {
             if (error) {
                 return console.log('Error', error);
             }
-            console.log(geoData.location);
-            console.log(`Hal-hazırda tempuratur ${data.tempC} °C-dir (${data.tempF} °F).`);
-            console.log(`${data.rainProb}% yağış yağma ehtimalı var`);
-            console.log(`${data.nowSum} hava müşahidə olunur`);
-            console.log(data.hourlySum);
+            console.log(place_name);
+            console.log(`Hal-hazırda tempuratur ${temperature} °C-dir (${tempF} °F).`);
+            console.log(`${precipProbability}% yağıntı ehtimalı var`);
+            console.log(`${summary} hava müşahidə olunur`);
+            console.log(hourlySum);
         })
     })
 }
--- weather-app/utils/forecast.js
+++ weather-app/utils/forecast.js
@@ -1,20 +1,29 @@
 const request = require('request');
 const forecast = (latitude, longitude, callback) => {
     const url = 'https://api.darksky.net/forecast/4b0f21b1962ec8cee7a7dede1f1c8076/' + latitude + ',' + longitude + '?units=si&lang=az'
-    request({ url: url, json: true }, (error, response) => {
+    request({ url, json: true }, (error, response) => {
         if (error) {
             callback('Unable to connect to forecast server!', undefined);
         } else if (response.body.error) {
             callback('Unable', undefined);
         } else {
-            const data = response.body;
+            const {
+                timezone,
+                currently,
+                hourly
+            } = response.body
+            const {
+                temperature,
+                precipProbability,
+                summary,
+            }=currently;
             callback(undefined, {
-                zone: data.timezone,
-                tempC: data.currently.temperature,
-                tempF: Math.floor((data.currently.temperature * 9 / 5 + 32) * 100) / 100,
-                rainProb: data.currently.precipProbability,
-                nowSum: data.currently.summary,
-                hourlySum: data.hourly.summary
+                timezone,
+                temperature,
+                tempF: Math.floor((temperature * 9 / 5 + 32) * 100) / 100,
+                precipProbability,
+                summary,
+                hourlySum: hourly.summary
             });
         }
     })
