f2b619fd7a578bd1f314c1c57e573bb6363cf415
Yunis Huseynzade <yunisdev.04@gmail.com>
2020-02-29 18:14:27 +0400

    weather

--- weather-app/app.js
+++ weather-app/app.js
@@ -1,31 +1,23 @@
-const request = require('request');
 const geoCode = require('./utils/geocode.js');
 const forecast = require('./utils/forecast.js');
-// const url = 'https://api.darksky.net/forecast/4b0f21b1962ec8cee7a7dede1f1c8076/40.4093,49.8671?units=si&lang=az'
-
-// request({
-//      url:url,
-//      json:true,
-//  },(error,response)=>{
-//      if(error){
-//          console.log(error);
-//      } else if(response.body.error){
-//          console.log(response.body.error);
-//      } else {
-//          const data = response.body;
-//          const temperature = data.currently.temperature;
-//          const prcsp = data.currently.precipProbability
-//          console.log(`Hal-hazırda tempuratur ${temperature} °C-dir (${temperature*9/5 + 32} °F).\n`+
-//                      `${prcsp}% yağış yağma ehtimalı var.`);
-//          console.log(data.currently.summary+" hava müşahidə olunur.");
-//          console.log(data.hourly.summary);
-//      }
-//  });
-forecast(40.36666,49.83518,(error,data)=>{
-    console.log('Error',error);
-    console.log('Data',data);
-})
-// geoCode('Baku',(error,data)=>{
-//     console.log('Error: ',error);
-//     console.log('Data: ',data);
-// })
+const address = process.argv[2];
+if (address) {
+    geoCode(address, (error, geoData) => {
+        if (error) {
+            return console.log('Error: ', error);
+        }
+        forecast(geoData.latitude, geoData.longitude, (error, data) => {
+            if (error) {
+                return console.log('Error', error);
+            }
+            console.log(geoData.location);
+            console.log(`Hal-hazırda tempuratur ${data.tempC} °C-dir (${data.tempF} °F).`);
+            console.log(`${data.rainProb}% yağış yağma ehtimalı var`);
+            console.log(`${data.nowSum} hava müşahidə olunur`);
+            console.log(data.hourlySum);
+        })
+    })
+}
+else {
+    console.log('Please provide a location');
+}
--- weather-app/utils/forecast.js
+++ weather-app/utils/forecast.js
@@ -1,20 +1,20 @@
 const request = require('request');
-const forecast = (longitude,latitude,callback) => {
-    const url = 'https://api.darksky.net/forecast/4b0f21b1962ec8cee7a7dede1f1c8076/'+latitude+','+longitude+'?units=si&lang=az'
+const forecast = (latitude, longitude, callback) => {
+    const url = 'https://api.darksky.net/forecast/4b0f21b1962ec8cee7a7dede1f1c8076/' + latitude + ',' + longitude + '?units=si&lang=az'
     request({ url: url, json: true }, (error, response) => {
         if (error) {
-            callback('Unable to connect to forecast server!',undefined);
+            callback('Unable to connect to forecast server!', undefined);
         } else if (response.body.error) {
-            callback('Unable',undefined);
-        }else{
+            callback('Unable', undefined);
+        } else {
             const data = response.body;
-            callback(undefined,{
-                zone:data.timezone,
-                tempC:data.currently.temperature,
-                tempF:Math.floor((data.currently.temperature*9/5 + 32)*100)/100,
-                rainProb:data.currently.precipProbability,
-                nowSum:data.currently.summary,
-                hourlySum:data.hourly.summary
+            callback(undefined, {
+                zone: data.timezone,
+                tempC: data.currently.temperature,
+                tempF: Math.floor((data.currently.temperature * 9 / 5 + 32) * 100) / 100,
+                rainProb: data.currently.precipProbability,
+                nowSum: data.currently.summary,
+                hourlySum: data.hourly.summary
             });
         }
     })
