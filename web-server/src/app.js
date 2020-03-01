d298e7bba739f306735f16be3e112293522742b8
Yunis Huseynzade <yunisdev.04@gmail.com>
2020-03-01 17:12:44 +0400

    express

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -1,23 +1,17 @@
+const path = require('path');
 const express = require('express');
 
 const app = express();
+app.use(express.static(path.join(__dirname,'../public')));
+app.set('view engine','hbs')
 
-app.get('',(req,res)=>{
-    res.send('hello express');
-
+app.get('/weather',(req,res)=>{
+    res.send({
+        location:'Philadelphia',
+        temperature:50
+    });
 })
 
-app.get('/help',(req,res)=>{
-    res.send('Help page');
-})
-
-app.get('/about',(req,res)=>{
-    res.send('About page');
-})
-
-//app.com
-//app.com/help
-//app.com/about
 
 app.listen(3000,()=>{
     console.log('Server is up on port 3000.');
