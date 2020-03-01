38f878a181084d006efe2feba118b91b50344761
Yunis Huseynzade <yunisdev.04@gmail.com>
2020-03-01 18:06:56 +0400

    404

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -1,18 +1,56 @@
 const path = require('path');
 const express = require('express');
+const hbs = require('hbs');
 
 const app = express();
-app.use(express.static(path.join(__dirname,'../public')));
-app.set('view engine','hbs')
 
-app.get('/weather',(req,res)=>{
-    res.send({
-        location:'Philadelphia',
-        temperature:50
+//Define paths for Express config
+const publicDirectoryPath = path.join(__dirname, '../public');
+const viewsPath = path.join(__dirname, '../templates/views');
+const partialsPath = path.join(__dirname, '../templates/partials')
+
+//Setup handlebars engine and vsiews location
+app.set('view engine', 'hbs');
+app.set('views', viewsPath);
+hbs.registerPartials(partialsPath)
+
+//Setup static directory to serve
+app.use(express.static(publicDirectoryPath));
+
+
+app.get('', (req, res) => {
+    res.render('index', {
+        title: 'Weather',
+        name: 'Yunis Huseynzade'
     });
 })
 
+app.get('/about', (req, res) => {
+    res.render('about', {
+        title: 'About',
+        name: 'Yunis Huseynzade'
+    })
+})
 
-app.listen(3000,()=>{
+app.get('/help', (req, res) => {
+    res.render('help', {
+        title: 'Help',
+        message: 'ahahafhs',
+        name: 'Yunis Huseynzade'
+    })
+})
+
+app.get('/weather', (req, res) => {
+    res.send({
+        location: 'Philadelphia',
+        temperature: 50
+    });
+})
+
+app.get('*',(req,res)=>{
+    res.send('My 404 page')
+})
+
+app.listen(3000, () => {
     console.log('Server is up on port 3000.');
 })
